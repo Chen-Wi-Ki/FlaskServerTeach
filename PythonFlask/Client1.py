a8ee3725a634169fe519895b140d73727fc0b605
陳威齊 <chenwici@stust.edu.tw>
2025-05-12 12:01:46 +0800

    Post Method--Bmi & Age & csv to judge

--- PythonFlask/Client1.py
+++ PythonFlask/Client1.py
@@ -4,8 +4,9 @@ url = 'http://127.0.0.1:5000/submit'
 payload = {'name': 'wiki',
            'password': '123456',
            'birthday':'1990-04-24',
+           'gender':'male',
            'height':1.75,
-           'weight':60.2}
+           'weight':110}
 
 response = requests.post(url, json=payload)
 
--- PythonFlask/Server1.py
+++ PythonFlask/Server1.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+import math
+import pandas as pd
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
 
 # POST Method
 @app.route('/submit', methods=['POST'])
@@ -17,12 +15,26 @@ def submit_form():
     gender = data.get('gender')
     height = data.get('height')
     weight = data.get('weight')
-    
-    # bmi count
-    bmi = round(weight / height **2,2)
-    
-    # age count
-    today = datetime.today()
+    bmi = round(weight / height **2,2)# bmi count
+    today = datetime.today()# age count
     age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
-
-    return jsonify(message=f'Your AGE={age},BMI={bmi}')
+    df = pd.read_csv('BMI_Normal.csv')
+    matched_rows = df[df['age'] == age].iloc[0]
+    print(matched_rows)
+    bmi_judge = 'wrong data'
+    if gender=='male':
+        if bmi<matched_rows['male_min']:
+            bmi_judge='Too thin'
+        elif bmi>=matched_rows['male_min'] and bmi<=matched_rows['male_max']:
+            bmi_judge='Normal'
+        else:
+            bmi_judge='Too fat'
+    elif gender=='female':
+        if bmi<matched_rows['female_min']:
+            bmi_judge='Too thin'
+        elif bmi>=matched_rows['female_min'] and bmi<=matched_rows['female_max']:
+            bmi_judge='Normal'
+        else:
+            bmi_judge='Too fat'
+        
+    return jsonify(message=f'Your AGE={age},BMI={bmi},{bmi_judge}.')
