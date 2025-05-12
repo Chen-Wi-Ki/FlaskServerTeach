d029f8b152123d6ed23a2cf25fbd85152800d308
陳威齊 <chenwici@stust.edu.tw>
2025-05-12 16:24:55 +0800

    Post Method--Bmi & Age & csv to judge & csv to UserData Save

--- PythonFlask/Client1.py
+++ PythonFlask/Client1.py
@@ -1,14 +1,11 @@
 import requests
-
-url = 'http://127.0.0.1:5000/submit'
+url = 'http://wiciar.com/bmi/submit'
 payload = {'name': 'wiki',
-           'password': '123456',
+           'password': 'abc123',
            'birthday':'1990-04-24',
            'gender':'male',
-           'height':1.75,
-           'weight':110}
-
+           'height':1.88,
+           'weight':44}
 response = requests.post(url, json=payload)
-
 print('Status Code:', response.status_code)
 print('Response JSON:', response.json())
--- PythonFlask/Server1.py
+++ PythonFlask/Server1.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from datetime import datetime
 import math
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -16,11 +17,11 @@ def submit_form():
     height = data.get('height')
     weight = data.get('weight')
     bmi = round(weight / height **2,2)# bmi count
-    today = datetime.today()# age count
+    today = datetime.today()
     age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
     df = pd.read_csv('BMI_Normal.csv')
     matched_rows = df[df['age'] == age].iloc[0]
-    print(matched_rows)
+    #print(matched_rows)
     bmi_judge = 'wrong data'
     if gender=='male':
         if bmi<matched_rows['male_min']:
@@ -36,5 +37,34 @@ def submit_form():
             bmi_judge='Normal'
         else:
             bmi_judge='Too fat'
-        
+    filename = f"UserData\{name}.csv"
+    if os.path.exists(filename): # file exists
+        load_df = pd.read_csv(filename)# file load
+        stored_password = str(load_df.iloc[0]['password'])# password load
+        if stored_password != password:# password incorrect
+            return jsonify(message="password error!"), 403
+        else:# password correct
+            user_data = { #Save Data
+                'height': height,
+                'weight': weight,
+                'bmi': bmi,
+                'bmi_judge':bmi_judge,
+                'build_time':today
+            }
+            load_df = pd.concat([load_df, pd.DataFrame([user_data])], ignore_index=True)
+            load_df.to_csv(filename, index=False)
+    else:# no files
+        user_data = {#Save Data
+            'password': password,
+            'birthday': birthday,
+            'gender': gender,
+            'height': height,
+            'weight': weight,
+            'bmi': bmi,
+            'bmi_judge':bmi_judge,
+            'build_time':today
+        }
+        save_df = pd.DataFrame([user_data])
+        save_filename = f"UserData\{name}.csv".replace("/", "_")
+        save_df.to_csv(save_filename, index=False)
     return jsonify(message=f'Your AGE={age},BMI={bmi},{bmi_judge}.')
